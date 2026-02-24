026920940ad33f099e276c4ccfc8027ebdf16254
mlanzillotta97 <marcolanzillotta3@gmail.com>
2026-02-24 18:01:44 +0100

    Esercizi Vari
    - Matrici
    - Random
    - Lista

---
+++ martedi_24_Febbraio/mattina/eserMatriciERandom/Program.cs
@@ -0,0 +1,270 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Esercizio1();
+        Esercizio2();
+        Esercizio3();
+        Esercizio4();
+        Esercizio5();
+        Esercizio6();
+    }
+
+    static void Esercizio1()
+    {
+        Random random = new Random();
+
+        // Genera un numero casuale tra 1 e 10
+        int numeroSegreto = random.Next(1, 11); // 11 perché il limite superiore è escluso
+
+        Console.Write("Indovina il numero (tra 1 e 10): ");
+        int tentativo = int.Parse(Console.ReadLine());
+
+        if (tentativo == numeroSegreto)
+        {
+            Console.WriteLine("Complimenti! Hai indovinato 🎉");
+        }
+        else
+        {
+            Console.WriteLine("Mi dispiace! Il numero era " + numeroSegreto);
+        }
+    }
+    
+
+    
+        static void Esercizio2()
+    {
+        Random random = new Random();
+
+        // Simula il lancio di due dadi (1-6)
+        int dado1 = random.Next(1, 7);
+        int dado2 = random.Next(1, 7);
+
+        int somma = dado1 + dado2;
+
+        Console.WriteLine("Risultato del primo dado: " + dado1);
+        Console.WriteLine("Risultato del secondo dado: " + dado2);
+        Console.WriteLine("Somma totale: " + somma);
+    }
+    
+
+        static void Esercizio3()
+    {
+        Random random = new Random();
+
+        const int minTemp = -5;   // temperatura minima possibile
+        const int maxTemp = 35;   // temperatura massima possibile
+
+        Console.Write("Quanti giorni vuoi simulare? ");
+        int giorni = int.Parse(Console.ReadLine());
+
+        int[] temperature = new int[giorni];
+
+        int somma = 0;
+        int tempMinima = int.MaxValue;
+        int tempMassima = int.MinValue;
+
+        // Generazione temperature
+        for (int i = 0; i < giorni; i++)
+        {
+            temperature[i] = random.Next(minTemp, maxTemp + 1);
+            somma += temperature[i];
+
+            if (temperature[i] < tempMinima)
+                tempMinima = temperature[i];
+
+            if (temperature[i] > tempMassima)
+                tempMassima = temperature[i];
+        }
+
+        // Calcolo media
+        double media = (double)somma / giorni;
+
+        // Output risultati
+        Console.WriteLine("\nTemperature generate:");
+        for (int i = 0; i < giorni; i++)
+        {
+            Console.WriteLine("Giorno " + (i + 1) + ": " + temperature[i] + "°C");
+        }
+
+        Console.WriteLine("\nTemperatura minima: " + tempMinima + "°C");
+        Console.WriteLine("Temperatura massima: " + tempMassima + "°C");
+        Console.WriteLine("Temperatura media: " + media.ToString("F2") + "°C");
+    }
+
+    //----------ESERCIZI MATRICE----------//
+
+        static void Esercizio4()
+    {
+        Console.Write("Inserisci numero di righe: ");
+        int righe = int.Parse(Console.ReadLine());
+
+        Console.Write("Inserisci numero di colonne: ");
+        int colonne = int.Parse(Console.ReadLine());
+
+        int[,] matrice = new int[righe, colonne];
+
+        // Inserimento valori
+        for (int i = 0; i < righe; i++)
+        {
+            for (int j = 0; j < colonne; j++)
+            {
+                Console.Write($"Inserisci valore per [{i},{j}]: ");
+                matrice[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int sommaTotale = 0;
+
+        Console.WriteLine("\nSomma delle righe:");
+        for (int i = 0; i < righe; i++)
+        {
+            int sommaRiga = 0;
+
+            for (int j = 0; j < colonne; j++)
+            {
+                sommaRiga += matrice[i, j];
+            }
+
+            Console.WriteLine($"Riga {i}: {sommaRiga}");
+            sommaTotale += sommaRiga;
+        }
+
+        Console.WriteLine("\nSomma delle colonne:");
+        for (int j = 0; j < colonne; j++)
+        {
+            int sommaColonna = 0;
+
+            for (int i = 0; i < righe; i++)
+            {
+                sommaColonna += matrice[i, j];
+            }
+
+            Console.WriteLine($"Colonna {j}: {sommaColonna}");
+        }
+
+        Console.WriteLine($"\nSomma totale degli elementi: {sommaTotale}");
+    }
+
+        static void Esercizio5()
+    {
+        const int dimensione = 4;
+        Random random = new Random();
+
+        int[,] matrice1 = new int[dimensione, dimensione];
+        int[,] matrice2 = new int[dimensione, dimensione];
+
+        int[] sommeRighe1 = new int[dimensione];
+        int[] sommeRighe2 = new int[dimensione];
+
+        // Generazione matrici
+        for (int i = 0; i < dimensione; i++)
+        {
+            for (int j = 0; j < dimensione; j++)
+            {
+                matrice1[i, j] = random.Next(1, 51);
+                matrice2[i, j] = random.Next(1, 51);
+            }
+        }
+
+        Console.WriteLine("MATRICE 1:");
+        StampaMatrice(matrice1, sommeRighe1);
+
+        Console.WriteLine("\nMATRICE 2:");
+        StampaMatrice(matrice2, sommeRighe2);
+
+        // Confronto righe
+        int vittorie1 = 0;
+        int vittorie2 = 0;
+
+        for (int i = 0; i < dimensione; i++)
+        {
+            if (sommeRighe1[i] > sommeRighe2[i])
+                vittorie1++;
+            else if (sommeRighe2[i] > sommeRighe1[i])
+                vittorie2++;
+        }
+
+        Console.WriteLine("\nRISULTATO FINALE:");
+
+        if (vittorie1 > vittorie2)
+            Console.WriteLine("La Matrice 1 vince con " + vittorie1 + " righe vincenti!");
+        else if (vittorie2 > vittorie1)
+            Console.WriteLine("La Matrice 2 vince con " + vittorie2 + " righe vincenti!");
+        else
+            Console.WriteLine("Pareggio!");
+    }
+
+    static void StampaMatrice(int[,] matrice, int[] sommeRighe)
+    {
+        int dimensione = matrice.GetLength(0);
+
+        for (int i = 0; i < dimensione; i++)
+        {
+            int somma = 0;
+
+            for (int j = 0; j < dimensione; j++)
+            {
+                Console.Write(matrice[i, j].ToString().PadLeft(4));
+                somma += matrice[i, j];
+            }
+
+            sommeRighe[i] = somma;
+            Console.Write("  | Somma riga: " + somma);
+            Console.WriteLine();
+        }
+    }
+
+
+    
+        static void Esercizio6()
+    {
+        const int dimensione = 5;
+        Random random = new Random();
+
+        int[,] matrice = new int[dimensione, dimensione];
+
+        // Generazione matrice
+        for (int i = 0; i < dimensione; i++)
+        {
+            for (int j = 0; j < dimensione; j++)
+            {
+                matrice[i, j] = random.Next(1, 21);
+            }
+        }
+
+        int sommaPrincipale = 0;
+        int sommaSecondaria = 0;
+
+        Console.WriteLine("MATRICE 5x5:\n");
+
+        for (int i = 0; i < dimensione; i++)
+        {
+            for (int j = 0; j < dimensione; j++)
+            {
+                Console.Write(matrice[i, j].ToString().PadLeft(4));
+
+                // Diagonale principale (i == j)
+                if (i == j)
+                    sommaPrincipale += matrice[i, j];
+
+                // Diagonale secondaria (i + j == dimensione - 1)
+                if (i + j == dimensione - 1)
+                    sommaSecondaria += matrice[i, j];
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nSomma diagonale principale: " + sommaPrincipale);
+        Console.WriteLine("Somma diagonale secondaria: " + sommaSecondaria);
+
+        if (sommaPrincipale > sommaSecondaria)
+            Console.WriteLine("La diagonale principale è maggiore.");
+        else if (sommaSecondaria > sommaPrincipale)
+            Console.WriteLine("La diagonale secondaria è maggiore.");
+        else
+            Console.WriteLine("Le due diagonali sono uguali.");
+    }
+}
---
+++ martedi_24_Febbraio/pomeriggio/eserciziLista/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+
+        static void Main()
+    {
+        ListaInteriVuoti();
+        ListaDieciNumeri();
+        ListaQuindiciNumeri();
+    }
+    static void ListaInteriVuoti()
+    {
+        // Crea una lista di interi vuota
+        List<int> numeri = new List<int>();
+
+        // Chiede all’utente di inserire 5 numeri interi
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Inserisci il numero {i + 1}: ");
+            int numero = int.Parse(Console.ReadLine());
+            numeri.Add(numero);
+        }
+
+        // Calcola due somme (esempio: primi 2 e ultimi 3 numeri)
+        int somma1 = 0;
+        int somma2 = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            somma1 += numeri[i];
+        }
+
+        for (int i = 2; i < numeri.Count; i++)
+        {
+            somma2 += numeri[i];
+        }
+
+        // Indica quale delle due somme è maggiore
+        if (somma1 > somma2)
+        {
+            Console.WriteLine("La somma dei primi 2 numeri è maggiore.");
+        }
+        else if (somma2 > somma1)
+        {
+            Console.WriteLine("La somma degli ultimi 3 numeri è maggiore.");
+        }
+        else
+        {
+            Console.WriteLine("Le due somme sono uguali.");
+        }
+
+        // Chiede quanti numeri rimuovere
+        Console.Write("Quanti numeri vuoi rimuovere? ");
+        int quanti = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < quanti; i++)
+        {
+            Console.Write("Inserisci il numero da rimuovere: ");
+            int daRimuovere = int.Parse(Console.ReadLine());
+
+            if (numeri.Contains(daRimuovere))
+            {
+                numeri.Remove(daRimuovere);
+                Console.WriteLine("Numero rimosso.");
+            }
+            else
+            {
+                Console.WriteLine("Numero non presente nella lista.");
+            }
+        }
+
+        // Stampa tutti i numeri presenti nella lista
+        Console.WriteLine("Numeri presenti nella lista:");
+        foreach (int n in numeri)
+        {
+            Console.WriteLine(n);
+        }
+    }
+
+    static void ListaDieciNumeri()
+    {
+        Random random = new Random();
+        List<int> numeri = new List<int>();
+
+        // Genera 10 numeri casuali tra 1 e 100
+        for (int i = 0; i < 10; i++)
+        {
+            numeri.Add(random.Next(1, 101));
+        }
+
+        // Stampa la lista
+        Console.WriteLine("Lista generata:");
+        foreach (int n in numeri)
+        {
+            Console.Write(n + " ");
+        }
+        Console.WriteLine();
+
+        // Chiede all’utente un numero da cercare
+        Console.Write("Inserisci un numero da cercare: ");
+        int numeroCercato = int.Parse(Console.ReadLine());
+
+        // Controlla se il numero è presente
+        if (numeri.Contains(numeroCercato))
+        {
+            int posizione = numeri.IndexOf(numeroCercato);
+            Console.WriteLine($"Numero trovato in posizione (indice): {posizione}");
+        }
+        else
+        {
+            Console.WriteLine("Numero non trovato nella lista.");
+        }
+
+        // Trova tutti i numeri pari
+        List<int> numeriPari = new List<int>();
+
+        foreach (int n in numeri)
+        {
+            if (n % 2 == 0)
+            {
+                numeriPari.Add(n);
+            }
+        }
+
+        // Stampa quanti e quali sono i numeri pari
+        Console.WriteLine($"Numeri pari trovati: {numeriPari.Count}");
+        Console.WriteLine("Elenco numeri pari:");
+
+        foreach (int pari in numeriPari)
+        {
+            Console.Write(pari + " ");
+        }
+    }
+
+    static void ListaQuindiciNumeri()
+    {
+        Random random = new Random();
+        List<int> numeri = new List<int>();
+
+        // Crea una lista di almeno 15 numeri casuali tra 1 e 20 (con duplicati possibili)
+        for (int i = 0; i < 15; i++)
+        {
+            numeri.Add(random.Next(1, 21));
+        }
+
+        // Stampa la lista originale
+        Console.WriteLine("Lista originale:");
+        foreach (int n in numeri)
+        {
+            Console.Write(n + " ");
+        }
+
+        Console.WriteLine();
+
+        // Rimuove i duplicati mantenendo solo i valori unici
+        List<int> numeriUnici = numeri.Distinct().ToList();
+
+        // Ordina la lista in ordine crescente
+        numeriUnici.Sort();
+
+        // Stampa la lista finale senza duplicati e ordinata
+        Console.WriteLine("Lista senza duplicati e ordinata:");
+        foreach (int n in numeriUnici)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}
